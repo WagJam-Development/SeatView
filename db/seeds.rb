3e8271dc486a6a1c6fca50ce2ddc7ab0a1d01ee5
jameshlee1 <james.h.lee3@outlook.com>
2023-08-17 12:08:41 -0700

    finished adding all venues

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,100 @@ venues = [
   {
     name: 'Coors Field',
     location: 'Denver, CO',
+  },
+  {
+    name: 'Dodger Stadium',
+    location: 'Los Angeles, CA',
+  },
+  {
+    name: 'Fenway Park',
+    location: 'Boston, MA',
+  },
+  {
+    name: 'Field of Dreams',
+    location: 'Dyersville, IA',
+  },
+  {
+    name: 'Globe Life Field',
+    location: 'Arlington, TX',
+  },
+  {
+    name: 'Great American Ball Park',
+    location: 'Cincinnati, OH',
+  },
+  {
+    name: 'Guaranteed Rate Field',
+    location: 'Chicago, IL',
+  },
+  {
+    name: 'Kauffman Stadium',
+    location: 'Kansas City, MO',
+  },
+  {
+    name: 'LoanDepot Park',
+    location: 'Miami, FL',
+  },
+  {
+    name: 'Minute Maid Park',
+    location: 'Houston, TX',
+  },
+  {
+    name: 'Nationals Park',
+    location: 'Washington, DC',
+  },
+  {
+    name: 'Oakland Coliseum',
+    location: 'Oakland, CA',
+  },
+  {
+    name: 'Oracle Park',
+    location: 'San Francisco, CA',
+  },
+  {
+    name: 'Oriole Park at Camden Yards',
+    location: 'Baltimore, MD',
+  },
+  {
+    name: 'Petco Park',
+    location: 'San Diego, CA',
+  },
+  {
+    name: 'PNC Park',
+    location: 'Pittsburgh, PA',
+  },
+  {
+    name: 'Progressive Field',
+    location: 'Cleveland, OH',
+  },
+  {
+    name: 'Rogers Centre',
+    location: 'Toronto, ON',
+  },
+  {
+    name: 'T-Mobile Park',
+    location: 'Seattle, WA',
+  },
+  {
+    name: 'Target Field',
+    location: 'Chicago, IL',
+  },
+  {
+    name: 'Tropicana Field',
+    location: 'Chicago, IL',
+  },
+  {
+    name: 'Truist Park',
+    location: 'Atlanta, GA',
+  },
+  {
+    name: 'Wrigley Field',
+    location: 'Chicago, IL',
+  },
+  {
+    name: 'Yankee Stadium',
+    location: 'Bronx, NY',
   }
+
 ]
 
 views = [
@@ -60,42 +153,197 @@ views = [
     venue_id:3  # Busch Stadium
   },
   {
-    section: 30,
-    row: 4,
-    seat: 38,
-    view: '',
+    section: 223,
+    row: 1,
+    seat: 15,
+    view: 'https://aviewfrommyseat.com/photos/RonSigPi-20230720063422.jpg',
     venue_id:4  # Chase Field
   },
   {
-    section: 44,
-    row: 5,
-    seat: 49,
-    view: '',
+    section: 110,
+    row: 14,
+    seat: 18,
+    view: 'https://aviewfrommyseat.com/photos/mboppy-20230816173253.jpg',
     venue_id:5  # Citi Field
   },
   {
-    section: 51,
-    row: 6,
-    seat: 52,
-    view: '',
+    section: 111,
+    row: 1,
+    seat: 7,
+    view: 'https://aviewfrommyseat.com/photos/anonymous-20230809123237.jpg',
     venue_id:6  # Citizens Bank Park
   },
   {
-    section: 45,
-    row: 22,
-    seat: 7,
-    view: '',
+    section: 123,
+    row: 12,
+    seat: 3,
+    view: 'https://aviewfrommyseat.com/photos/anonymous-20230722213927.jpg',
     venue_id:7  # Comerica Park
   },
   {
-    section: 45,
-    row: 22,
-    seat: 7,
-    view: '',
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
     venue_id:8  # Coors Field
   },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:9  # Dodger Stadium
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:10  # Fenway Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:11  # Field of Dreams
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:12  # Globe Life Field
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:13  # Guaranteed Rate Field
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:14  # Kauffman Stadium
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:15  # LoanDepot Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:16  # Minute Maid Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:17  # Nationals Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:18  # Oakland Coliseum
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:19  # Oracle Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:20 # Oriole Park at Camden Yards
+    },  
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:21  # Petco Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:22  # PNC Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:23  # Progressive Field
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:24  # Rogers Centre
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:25  # T-Mobile Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:26  # Target Field
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:27  # Tropicana Field
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:28  # Truist Park
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:29  # Wrigley Field
+  },
+  {
+    section: 133,
+    row: 29,
+    seat: 5,
+    view: 'https://aviewfrommyseat.com/photos/Bobad76-20230705171847.jpg',
+    venue_id:30  # Yankee Stadium
+  }
 
 ]
+
 
 
 
